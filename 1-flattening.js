6c2ff80ab8ae144f9d61b2a3365b60d075a1e3e0
Finn Poduska <finnpoduska@gmail.com>
2024-02-15 09:00:40 -0800

    sadge

--- 1-flattening.js
+++ 1-flattening.js
@@ -1,5 +1,10 @@
 let arrays = [[1, 2, 3], [4, 5], [6]];
-// Your code here.
 
+let newArray;
+newArray.concat(arrays.reduce((accumulator, array) => {
+    newArray = newArray.concat(accumulator);
+    return accumulator = array;
+}, 0));
 
+console.log(newArray);
 // → [1, 2, 3, 4, 5, 6]
